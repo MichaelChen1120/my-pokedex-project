e259be94a2630e60f060952bad3046f7656aaf4c
Michael Chen <michael.chen.1120@gmail.com>
2016-08-10 19:45:56 +0000

    built basic login form

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,3 +1,16 @@
 class SessionsController < ApplicationController
 
+  get '/login' do
+    erb :login
+  end
+
+  post '/sessions' do
+    session[:username] = params[:username]
+    redirect '/user'
+  end
+
+  get '/logout' do
+    session.clear
+    redirect '/login'
+  end
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,3 +1,7 @@
 class UsersController < ApplicationController
 
+  get '/user' do
+    "You are logged in as #{session[:username]}"
+  end
+
 end
