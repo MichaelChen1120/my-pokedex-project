053a4dd49d5f5df7bf2b4128df450120d2338631
Michael Chen <michael.chen.1120@gmail.com>
2016-08-11 20:20:21 +0000

    finished pokemon scraper

--- app/models/pokemon.rb
+++ app/models/pokemon.rb
@@ -1,8 +1,19 @@
 class Pokemon <ActiveRecord::Base
   belongs_to :user
+  require 'nokogiri'
+  require 'open-uri'
 
   def self.scrape_pokemon
-    doc=Nokogiri::HTML(open("http://www.ign.com/wikis/pokemon-go/List_of_Pokemon_(Pokedex)"))
+    doc= Nokogiri::HTML(open("http://www.ign.com/wikis/pokemon-go/List_of_Pokemon_(Pokedex)"))
+    pokemon= doc.search("table a").collect do |pokemon|
+      pokemon.text
+    end
+    pokemon.reject! {|p| p.empty?}
+    pokemon.reject! {|p| p.include?("Evolutions")}
+    pokemon.reject! {|p| p.include?("Rainer")}
+    pokemon.reject! {|p| p.include?("Sparky")}
+    pokemon.reject! {|p| p.include?("Pyro")}
+    end
   end
 
 end
