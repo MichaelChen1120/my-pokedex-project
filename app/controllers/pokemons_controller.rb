3a1ed7bd059b68d569701927b2a8814cce60f6de
Michael Chen <michael.chen.1120@gmail.com>
2016-08-12 18:11:01 +0000

    built edit and delete methods not patching properly yet

--- app/controllers/pokemons_controller.rb
+++ app/controllers/pokemons_controller.rb
@@ -40,11 +40,31 @@ class PokemonsController < ApplicationController
     if !logged_in?
       redirect '/login'
     else
-      if  pokemon=current_user.pokemons.find_by(params[:id])
-        "An edit Pokemon form #{current_user.id} is editing #{pokemon.id}"
-      else
-        redirect '/pokemon'
+      @pokemons=Pokemon.find_by_id(params[:id])
+      erb :'/pokemons/edit'
+    end
+  end
+
+  patch '/pokemon/:id' do
+    @pokemons = Pokemon.find_by_id(params[:id])
+    @pokemons.nickname = params[:nickname]
+    @pokemons.weight = params[:weight]
+    @pokemons.height  =params[:height]
+    @pokemons.cp = params[:cp]
+    @pokemons.save
+    redirect "/pokemon/#{@pokemons.id}"
+  end
+
+  get '/pokemon/:id/delete' do
+    if !logged_in?
+      redirect '/login'
+    else
+      @user=current_user
+      @pokemons=Pokemon.find_by_id(params[:id])
+      if @pokemons.user_id == @user.id
+        @pokemons.delete
       end
+      redirect '/pokemon'
     end
   end
 
