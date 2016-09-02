c7a4308cc70e238bada8800138b1ebc9a02671b3
Michael Chen <michael.chen.1120@gmail.com>
2016-09-02 18:20:28 +0000

    removed redundant code in post users route

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -9,13 +9,9 @@ class UsersController < ApplicationController
   end
 
   post '/users' do
-    if params[:username].empty? || params[:password].empty?
-      redirect '/signup?error=fill in empty fields'
-    else
-      @user=User.create(username: params[:username], password: params[:password])
-      session[:user_id]= @user.id
-      redirect '/pokemon'
-    end
+    @user=User.create(username: params[:username], password: params[:password])
+    session[:user_id]= @user.id
+    redirect '/pokemon'
   end
 
   get '/login' do
